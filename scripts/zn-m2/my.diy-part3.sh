3abdd1bd67566f59fc096b2e517e4f2063a3c2c0
1 <1>
2025-04-12 10:38:50 +0800

    u

--- scripts/zn-m2/my.diy-part3.sh
+++ scripts/zn-m2/my.diy-part3.sh
@@ -14,8 +14,9 @@
 sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 
 #修改版本信息
-sed -i "s/DISTRIB_DESCRIPTION='*.*'/DISTRIB_DESCRIPTION='OpenWrt IPQ6000 ZN-M2 (build time: $(date +%Y%m%d))'/g"  package/base-files/files/etc/openwrt_release
-# 替换golang版本为1.23
+sed -i "s/DISTRIB_DESCRIPTION='*.*'/DISTRIB_DESCRIPTION='OpenWrt (build time: $(date +%Y%m%d))'/g"  package/extra/default-settings/files/99-default-settings
+
+# 替换golang版本为1.24
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
 
