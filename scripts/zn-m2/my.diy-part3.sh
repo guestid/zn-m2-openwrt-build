ecf769577cf39c0583a78da3ff58469898eeb14f
1 <1>
2025-04-14 11:10:00 +0800

    u

--- scripts/zn-m2/my.diy-part3.sh
+++ scripts/zn-m2/my.diy-part3.sh
@@ -14,7 +14,7 @@
 sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 
 #修改版本信息
-sed -i "s/DISTRIB_DESCRIPTION='*.*'/DISTRIB_DESCRIPTION='OpenWrt (build : $(date +%Y%m%d))'/g"  package/extra/default-settings/files/99-default-settings
+sed -i "s/DISTRIB_DESCRIPTION='*.*'/DISTRIB_DESCRIPTION='OpenWrt (B$(date +%Y%m%d))'/g"  package/extra/default-settings/files/99-default-settings
 
 # 替换golang版本为1.24
 rm -rf feeds/packages/lang/golang
@@ -27,4 +27,4 @@ git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/l
 sed -i -r "s#navbar_proxy = 'openclash'#navbar_proxy = 'passwall'#g" feeds/luci/themes/luci-theme-design/luasrc/view/themes/design/header.htm
 
 # 修改 argon 为默认主题,可根据你喜欢的修改成其他的（不选择那些会自动改变为默认主题的主题才有效果）
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
