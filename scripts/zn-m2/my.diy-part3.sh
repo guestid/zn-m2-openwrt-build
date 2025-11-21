9eb67aef2a353955c59b04366d1feb99f1de9f89
1 <1>
2025-11-21 11:22:40 +0800

    1

--- scripts/zn-m2/my.diy-part3.sh
+++ scripts/zn-m2/my.diy-part3.sh
@@ -14,11 +14,10 @@
 sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 
 #修改版本信息
-sed -i "s/DISTRIB_DESCRIPTION='*.*'/DISTRIB_DESCRIPTION='OpenWrt (B$(date +%Y%m%d))'/g"  package/extra/default-settings/files/99-default-settings
-
-# 替换golang版本为1.24
+sed -i "s/DISTRIB_DESCRIPTION='*.*'/DISTRIB_DESCRIPTION='OpenWrt IPQ6000 ZN-M2 (build time: $(date +%Y%m%d))'/g"  package/base-files/files/etc/openwrt_release
+# 替换golang版本为1.25
 rm -rf feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 25.x feeds/packages/lang/golang
 
 # ttyd免登陆
 #sed -i -r 's#/bin/login#/bin/login -f root#g' feeds/packages/utils/ttyd/files/ttyd.config
